517b2c792a282dda8e4a04ffaec14dfeae601441
Alexey Ozeritskiy <aozeritsky@gmail.com>
2022-06-02 00:11:04 +0300

    Use int as integer

--- contrib/f2c.h
+++ contrib/f2c.h
@@ -26,15 +26,15 @@ typedef char integer1;
 
 #else
 
-typedef long int integer;
-typedef unsigned long int uinteger;
+typedef int integer;
+typedef unsigned int uinteger;
 typedef char *address;
 typedef short int shortint;
 typedef float real;
 typedef double doublereal;
 typedef struct { real r, i; } complex;
 typedef struct { doublereal r, i; } doublecomplex;
-typedef long int logical;
+typedef int logical;
 typedef short int shortlogical;
 typedef char logical1;
 typedef char integer1;
